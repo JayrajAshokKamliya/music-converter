555c463816ec9efe53578b6b857bb31bd935a039
JayrajAshokKamliya <jayrajkamliyal@gmail.com>
2026-02-21 11:39:11 +0530

    Added first HelloController

--- src/main/java/com/jay/musicconverter/HelloController.java
+++ src/main/java/com/jay/musicconverter/HelloController.java
@@ -1,5 +1,12 @@
 package com.jay.musicconverter;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
-    
+    @GetMapping("/hello")
+    public String hello(){
+        return "Starting a new project";
+    }
 }
